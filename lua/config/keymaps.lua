06c2dfa80341d00101e0248b22903a7a5073199d
ahacad <ahacad@126.com>
2023-10-14 22:40:47 -0700

    feat: init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local Util = require("lazyvim.util")
+
+local map = Util.safe_keymap_set
+
+map("n", "J", "5j")
+map("x", "J", "5j")
+map("n", "K", "5k")
+map("x", "K", "5k")
+
+map("n", "Q", ":q<CR>")
