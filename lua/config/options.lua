7fb71fb6c3bab1463651ede32ef6f45502b798fc
ahacad <ahacad@126.com>
2023-10-14 22:56:35 -0700

    feat: update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,4 +4,8 @@
 
 local opt = vim.opt
 
-opt.relativenumber = false -- Absolute linu numbers
+opt.relativenumber = false -- absolute linu numbers
+opt.tabstop = 4 --  number of space tabs count for
+opt.shiftwidth = 4 -- size of a indent
+opt.softtabstop = 4 -- number of columns the cursor moves right when pressing tab
+vim.cmd("set guicursor=a:block") -- always use block in typing, block is better :)
